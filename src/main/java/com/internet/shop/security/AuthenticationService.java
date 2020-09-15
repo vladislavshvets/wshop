23b1ec0ec272dc5b8cce0c6cbfef9ec5c422fdc5
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-09-15 14:07:07 +0300

    fixed code-style

--- src/main/java/com/internet/shop/security/AuthenticationService.java
+++ src/main/java/com/internet/shop/security/AuthenticationService.java
@@ -3,8 +3,6 @@ package com.internet.shop.security;
 import com.internet.shop.exceptions.AuthenticationException;
 import com.internet.shop.model.User;
 
-import java.util.Optional;
-
 public interface AuthenticationService {
     User login(String login, String password) throws AuthenticationException;
 }
