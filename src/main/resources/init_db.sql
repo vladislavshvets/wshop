87255735186fcd1370ebdc72df21d5618cf71b0f
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-09-21 11:02:19 +0300

    fixed init_db

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -4,9 +4,7 @@ CREATE TABLE `products` (
   `name` varchar(225) NOT NULL,
   `price` double NOT NULL,
   `deleted` tinyint NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name_UNIQUE` (`name`) )
-  ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`), )
 
 INSERT INTO products(name, price)
 VALUES ('cherry', '33,5'),
